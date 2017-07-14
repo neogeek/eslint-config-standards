71fb1d48b45b1549269ab52668adbbef1a30ef2f
Scott Doxey <neogeek0101@gmail.com>
2017-07-13 22:02:01 -0400

    Switched from cheerio to JSDOM.

--- utils/update.js
+++ utils/update.js
@@ -1,37 +1,25 @@
-/* eslint arrow-body-style: 0 */
 /* eslint global-require: 0 */
 /* eslint no-sync: 0 */
 
 const fs = require('fs');
 const path = require('path');
 
-const cheerio = require('cheerio');
+const {JSDOM} = require('jsdom');
 const request = require('request');
 
 const SPACE_SIZE = 2;
 
-const parseRulesFromTable = ($body, id) => {
+const parseRulesFromTable = (doc, id) => {
 
-    const rules = $body(id)
-        .nextAll('.rule-list')
-        .eq(0)
-        .find('td:nth-of-type(3)')
-        .toArray()
-        .map(rule =>
-            $body(rule).text()
-                .trim()
-        );
+    const rules = [].slice.call(doc.querySelector(`#${id} ~ .rule-list`).querySelectorAll('td:nth-of-type(3)')).map(rule =>
+        rule.querySelector('a').textContent.trim());
 
     const customRulesFile = path.join(__dirname, `./mods/eslintrc-${id.replace(/^#/, '')}.json`);
 
     const customRules = require(customRulesFile).rules;
 
     return {
-        'rules': Object.assign({}, ...rules.map(rule => {
-
-            return {[rule]: 2};
-
-        }), customRules)
+        'rules': Object.assign({}, ...rules.map(rule => ({[rule]: 2})), customRules)
     };
 
 };
@@ -44,19 +32,19 @@ request('http://eslint.org/docs/rules/', (err, res, body) => {
 
     }
 
-    const $body = cheerio.load(body);
+    const {document} = new JSDOM(body).window;
 
     return [
-        '#best-practices',
-        '#ecmascript-6',
-        '#nodejs-and-commonjs',
-        '#possible-errors',
-        '#strict-mode',
-        '#stylistic-issues',
-        '#variables'
+        'best-practices',
+        'ecmascript-6',
+        'nodejs-and-commonjs',
+        'possible-errors',
+        'strict-mode',
+        'stylistic-issues',
+        'variables'
     ].forEach(id => fs.writeFileSync(
-        `.eslintrc-${id.replace(/^#/, '')}`,
-        `${JSON.stringify(parseRulesFromTable($body, id), null, SPACE_SIZE)}\n`
+        `.eslintrc-${id}`,
+        `${JSON.stringify(parseRulesFromTable(document, id), null, SPACE_SIZE)}\n`
     ));
 
 });
