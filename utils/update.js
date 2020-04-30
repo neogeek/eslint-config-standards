007a8ef02908aa92cb8868c62c6100b2adc72783
Scott Doxey <neogeek0101@gmail.com>
2020-04-29 22:42:08 -0400

    Formatted file.

--- utils/update.js
+++ utils/update.js
@@ -4,71 +4,59 @@
 const fs = require('fs');
 const path = require('path');
 
-const {JSDOM} = require('jsdom');
+const { JSDOM } = require('jsdom');
 const request = require('request');
 
 const SPACE_SIZE = 2;
 
 const parseRulesFromTable = (document, id) => {
-
     const rules = [].slice
-        .call(document
-            .querySelector(`#${id} ~ .rule-list`)
-            .querySelectorAll('td:nth-of-type(3)'))
+        .call(
+            document
+                .querySelector(`#${id} ~ .rule-list`)
+                .querySelectorAll('td:nth-of-type(3)')
+        )
         .map(rule => rule.querySelector('a').textContent.trim());
 
     const customRulesFile = path.join(
         __dirname,
-        `./mods/eslintrc-${id.replace(
-            /^#/u,
-            ''
-        )}.json`
+        `./mods/eslintrc-${id.replace(/^#/u, '')}.json`
     );
 
     const customRules = require(customRulesFile).rules;
 
     return {
-        'rules': Object.assign(
+        rules: Object.assign(
             {},
-            ...rules.map(rule => ({[rule]: 2})),
+            ...rules.map(rule => ({ [rule]: 2 })),
             customRules
         )
     };
-
 };
 
-request(
-    'http://eslint.org/docs/rules/',
-    (err, res, body) => {
-
-        if (err) {
-
-            return new Error(err);
-
-        }
-
-        const {document} = new JSDOM(body).window;
-
-        return [
-            'best-practices',
-            'ecmascript-6',
-            'nodejs-and-commonjs',
-            'possible-errors',
-            'strict-mode',
-            'stylistic-issues',
-            'variables'
-        ].forEach(id =>
-            fs.writeFileSync(
-                `.eslintrc-${id}`,
-                `${JSON.stringify(
-                    parseRulesFromTable(
-                        document,
-                        id
-                    ),
-                    null,
-                    SPACE_SIZE
-                )}\n`
-            ));
-
+request('http://eslint.org/docs/rules/', (err, res, body) => {
+    if (err) {
+        return new Error(err);
     }
-);
+
+    const { document } = new JSDOM(body).window;
+
+    return [
+        'best-practices',
+        'ecmascript-6',
+        'nodejs-and-commonjs',
+        'possible-errors',
+        'strict-mode',
+        'stylistic-issues',
+        'variables'
+    ].forEach(id =>
+        fs.writeFileSync(
+            `.eslintrc-${id}`,
+            `${JSON.stringify(
+                parseRulesFromTable(document, id),
+                null,
+                SPACE_SIZE
+            )}\n`
+        )
+    );
+});
